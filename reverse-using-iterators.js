1f75e36764208079d9037d3f1ac50a66b8c521f2
Deepak Acharya <aacharyadeepak100@gmail.com>
2018-07-09 14:46:17 +0530

    updated

--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,6 +1,6 @@
 // The intent of this file is to reverse the string using iterators (for, foreach)
 const reverse = (string) => {
-    var result=""
+    var result="";
     for(var ind=string.length-1; ind>=0; ind--){
         result+=string[ind];
     }
