f4d1be88c29f3270776d6109580bc3d99caa3fcd
Deepak Acharya <aacharyadeepak100@gmail.com>
2018-07-09 15:07:08 +0530

    updated

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,8 +1,8 @@
 // The intent of this file is to reverse
 // the string using built in functions.
 const reverse = (string) => {
-    var list1=string.split("");
-    var reversedList=list1.reverse();
+    var listString=string.split("");
+    var reversedList=listString.reverse();
     return reversedList.join("");
 };
 
